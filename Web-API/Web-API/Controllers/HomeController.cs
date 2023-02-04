e26f67f58b201b2b288f0947f5ac72f5dcb6210a
satishgithub940 <83502527+satishgithub940@users.noreply.github.com>
2023-02-04 13:09:23 +0530

    new project setup

---
+++ Web-API/Web-API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string name = "home page";
+            return Ok(name);
+        }
+    }
+}
